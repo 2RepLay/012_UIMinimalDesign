0bf606822183b4181e7715953b313c59c550c781
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-22 20:10:18 +0300

    Initial Commit

---
+++ 012_UIMinimalDesign/_12_UIMinimalDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  _12_UIMinimalDesignApp.swift
+//  012_UIMinimalDesign
+//
+//  Created by nikita on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _12_UIMinimalDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
